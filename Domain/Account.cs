6d0713ed73771a8011d20aa49fde2f158537b43e
Martin Leeb <m.leeb.it@gmail.com>
2020-11-17 15:13:44 +0100

    change user manager framework

---
+++ Domain/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Account
+    {
+        public readonly string UserName;
+
+        string Password;
+
+        public readonly string FirstName;
+
+        public readonly string LastName;
+
+        public Account(string userName, string password, string firstName, string lastName)
+        {
+            UserName = userName;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void ChangePassword(string newPassword)
+        {
+            Password = newPassword;
+        }
+    }
+}
